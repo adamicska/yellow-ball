bc270ed58ab5776130a722d002a9813d0dd83ac0
adamicska <me@adamicska.dev>
2024-01-11 11:58:51 -0300

    logo path fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import logo from "../public/images/fire-ball.png";
+import logo from "../../public/images/fire-ball.png";
 
 const features = [
   {
